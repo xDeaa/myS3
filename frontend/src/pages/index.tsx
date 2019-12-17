0f63714d812ed38ad21eef2fd865f722e6c7463e
xDeaa <andrea_christina@hotmail.fr>
2019-12-17 16:21:22 +0100

    bigin uplod file and index for page

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Home from "./Home"
+import BucketsPage from "./BucketsPage"
+import LoginPage from "./LoginPage"
+import RegisterPage from "./RegisterPage"
+import BlobPage from "./BlobPage"
+import Page404 from "./Page404"
+
+export { Home, BucketsPage, LoginPage, RegisterPage, BlobPage, Page404 }
